86a4189e180e707b62cd0515f5e4be5971a17f90
bentesf <felipe.bentes84@gmail.com>
2022-10-11 16:52:43 -0400

    Novas bases

---
+++ lib/anchor_scroll_controller.dart
@@ -0,0 +1,2 @@
+export 'src/anchor_scroll_controller.dart' show AnchorScrollController;
+export 'src/anchor_scroll_wrapper.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_barcodes/barcodes.dart';
+
+import 'home.dart';
 
 void main() {
-  return runApp(MyApp());
+  runApp(MyApp());
 }
 
-/// Creates the barcode generator
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text('Barcode Generator Demo'),
-            ),
-            body: ListView(
-              children: <Widget>[
-                Container(
-                  child: Center(
-                      child: Container(
-                    height: 150,
-                    margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                    child: SfBarcodeGenerator(
-                      value: '123456789',
-                      symbology: Codabar(),
-                      showValue: true,
-                    ),
-                  )),
-                )
-              ],
-            )));
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
   }
 }
-
-// import 'package:automatic_animated_list/automatic_animated_list.dart';
-// import 'package:flutter/material.dart';
-// import 'package:syncfusion_flutter_barcodes/barcodes.dart';
-
-// void main() {
-//   return runApp(MyApp());
-// }
-
-// /// Creates the barcode generator
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//           appBar: AppBar(
-//             title: const Text('Barcode Generator Demo'),
-//           ),
-//           body: ListView(children: <Widget>[
-//             Container(
-//               height: 150,
-//               margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-//               child: SfBarcodeGenerator(
-//                 value: '123456',
-//                 symbology: Codabar(module: 4),
-//                 showValue: true,
-//               ),
-//             ),
-//           ])),
-//     );
-//   }
-// }
