f9f6094564812de718248c69ba99c6d037afd253
bentesf <felipe.bentes84@gmail.com>
2022-10-26 09:14:48 -0400

    Novas funcionalidades

--- lib/general_scroll_view.dart
+++ lib/general_scroll_view.dart
@@ -1,6 +1,7 @@
 import 'package:anchor_scroll_controller/anchor_scroll_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:syncfusion_flutter_barcodes/barcodes.dart';
 import 'dart:convert';
 import 'dart:io';
@@ -20,11 +21,12 @@ class _GeneralScrollViewWidgetState extends State<GeneralScrollViewWidget> {
   late List<List<dynamic>> listaCodigos;
   late List<List<dynamic>> blocoCodigo;
   late int contPage;
-  late int tanAnt;
+  late int tanBloc;
   bool isLoading = false;
   late final AnchorScrollController _scrollController;
   final key = new GlobalKey<ScaffoldState>();
   TabController? _tabController;
+  final ctrlSenha = TextEditingController();
 
   List<PlatformFile>? _paths;
   String? _extension = "csv";
@@ -36,6 +38,7 @@ class _GeneralScrollViewWidgetState extends State<GeneralScrollViewWidget> {
     listaCodigos = List<List<dynamic>>.empty(growable: true);
     blocoCodigo = List<List<dynamic>>.empty(growable: true);
     contPage = 0;
+    tanBloc = 0;
     _scrollController = AnchorScrollController(
       onIndexChanged: (index, userScroll) {
         if (userScroll) {
@@ -48,32 +51,65 @@ class _GeneralScrollViewWidgetState extends State<GeneralScrollViewWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          leading: const Icon(Icons.format_list_bulleted_sharp),
-          title: Text("Codigos - Produtos"),
-          actions: <Widget>[
-            IconButton(
-                onPressed: _openFileExplorer, icon: Icon(Icons.download)),
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    blocoCodigo = [];
-                    listaCodigos = [];
-                    _paths = [];
-                    contPage = 0;
-                  });
-                },
-                icon: Icon(Icons.restore_from_trash)),
-          ]),
+      appBar: AppBar(title: Text("Codigos - Produtos"), actions: <Widget>[
+        IconButton(onPressed: _openFileExplorer, icon: Icon(Icons.download)),
+        IconButton(
+            onPressed: () {
+              setState(() {
+                blocoCodigo = [];
+                listaCodigos = [];
+                _paths = [];
+                contPage = 0;
+                tanBloc = 0;
+              });
+            },
+            icon: Icon(Icons.restore_from_trash)),
+      ]),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         mainAxisSize: MainAxisSize.max,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Center(
-              child: Container(
-            child: Text("Pagina: " + contPage.toString()),
-          )),
+          Container(
+            color: Colors.white,
+            height: 40,
+            alignment: Alignment.centerLeft,
+            child: Container(
+              height: 30,
+              child: DefaultTabController(
+                animationDuration: Duration(seconds: 1),
+                length: blocoCodigo.length,
+                child: Builder(builder: (context) {
+                  _tabController = DefaultTabController.of(context);
+                  return TabBar(
+                      isScrollable: true,
+                      tabs: List.generate(
+                          blocoCodigo.length,
+                          (index) => Container(
+                                width: 50,
+                                alignment: Alignment.center,
+                                child: Text(
+                                  (index).toString(),
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              )),
+                      labelPadding: EdgeInsets.symmetric(horizontal: 5),
+                      indicatorSize: TabBarIndicatorSize.label,
+                      indicator: BoxDecoration(
+                        color: Color.fromARGB(255, 21, 116, 180),
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                      ),
+                      onTap: (index) {
+                        _scrollController.scrollToIndex(
+                            index: index, scrollSpeed: 0.1);
+                      });
+                }),
+              ),
+            ),
+          ),
           Expanded(
               child: ListView.builder(
                   controller: _scrollController,
@@ -96,7 +132,7 @@ class _GeneralScrollViewWidgetState extends State<GeneralScrollViewWidget> {
                             // color: Colors.blue,
                             height: 160,
                             width: 300,
-                            margin: EdgeInsets.fromLTRB(40, 60, 20, 60),
+                            margin: EdgeInsets.fromLTRB(40, 60, 40, 60),
                             child: SfBarcodeGenerator(
                               value: blocoCodigo[index][0],
                               symbology: Codabar(module: 6),
@@ -105,46 +141,35 @@ class _GeneralScrollViewWidgetState extends State<GeneralScrollViewWidget> {
                           )),
                         ),
                       )))),
-          Container(
-            color: Colors.white,
-            height: 40,
-            alignment: Alignment.centerLeft,
-            child: Container(
-              height: 30,
-              child: DefaultTabController(
-                animationDuration: Duration(seconds: 1),
-                length: blocoCodigo.length,
-                child: Builder(builder: (context) {
-                  _tabController = DefaultTabController.of(context);
-                  return TabBar(
-                      isScrollable: true,
-                      tabs: List.generate(
-                          blocoCodigo.length,
-                          (index) => Container(
-                                width: 50,
-                                alignment: Alignment.center,
-                                child: Text(
-                                  (index + 1).toString(),
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 18,
-                                  ),
-                                ),
-                              )),
-                      labelPadding: EdgeInsets.symmetric(horizontal: 5),
-                      indicatorSize: TabBarIndicatorSize.label,
-                      indicator: BoxDecoration(
-                        color: Color.fromARGB(255, 21, 116, 180),
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                      ),
-                      onTap: (index) {
-                        _scrollController.scrollToIndex(
-                            index: index + 1, scrollSpeed: 0.1);
-                      });
-                }),
+          IconButton(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 50),
+              icon: const Icon(
+                Icons.play_circle_outlined,
+                color: Colors.blue,
+                size: 50,
               ),
+              onPressed: () {
+                _scrollController.scrollToIndex(
+                    index: tanBloc - 1, scrollSpeed: 0.1);
+                // setState(() {
+                _tabController?.animateTo(0,
+                    duration: Duration(seconds: 6 * (tanBloc - 1)));
+                // });
+              }
+              // Fluttertoast.showToast(msg: 'Dummy search action.'),
+              ),
+          Center(
+              child: Container(
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
+            child: Text(
+              "BLOCO: " + contPage.toString(),
+              style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                  fontSize: 16.0),
             ),
-          )
+          )),
         ],
       ),
       floatingActionButton: SpeedDial(
@@ -158,9 +183,12 @@ class _GeneralScrollViewWidgetState extends State<GeneralScrollViewWidget> {
           SpeedDialChild(
             child: isLoading
                 ? CircularProgressIndicator(
-                    color: Colors.black,
+                    color: Colors.white,
                   )
-                : const Icon(Icons.add),
+                : const Icon(
+                    Icons.settings_backup_restore_rounded,
+                    color: Colors.white,
+                  ),
             backgroundColor: Color.fromARGB(255, 43, 118, 231),
             onTap: () {
               setState(() {
@@ -185,9 +213,12 @@ class _GeneralScrollViewWidgetState extends State<GeneralScrollViewWidget> {
           SpeedDialChild(
             child: isLoading
                 ? CircularProgressIndicator(
-                    color: Colors.black,
+                    color: Colors.white,
                   )
-                : const Icon(Icons.add),
+                : const Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
             backgroundColor: Color.fromARGB(255, 43, 118, 231),
             onTap: () {
               setState(() {
@@ -207,6 +238,35 @@ class _GeneralScrollViewWidgetState extends State<GeneralScrollViewWidget> {
                 color: Colors.white,
                 fontSize: 16.0),
             labelBackgroundColor: Color.fromARGB(255, 43, 118, 231),
+          ),
+          SpeedDialChild(
+            child: isLoading
+                ? CircularProgressIndicator(
+                    color: Colors.white,
+                  )
+                : const Icon(
+                    Icons.format_list_numbered_rtl_rounded,
+                    color: Colors.white,
+                  ),
+            backgroundColor: Color.fromARGB(255, 43, 118, 231),
+            onTap: () {
+              setState(() {
+                isLoading = true;
+              });
+              Future.delayed(const Duration(seconds: 3), () {
+                _onAlertWithCustomContentPressed(context);
+
+                setState(() {
+                  isLoading = false;
+                });
+              });
+            },
+            label: 'Quantidade',
+            labelStyle: TextStyle(
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+                fontSize: 16.0),
+            labelBackgroundColor: Color.fromARGB(255, 43, 118, 231),
           )
         ],
       ),
@@ -221,28 +281,24 @@ class _GeneralScrollViewWidgetState extends State<GeneralScrollViewWidget> {
       color: Theme.of(context).primaryColor,
       child: Row(
         children: <Widget>[
-          // Bottom that pops up from the bottom of the screen.
-          // IconButton(
-          //   icon: const Icon(Icons.menu),
-          //   onPressed: () => showModalBottomSheet(
-          //     context: context,
-          //     builder: (BuildContext context) => Container(
-          //       alignment: Alignment.center,
-          //       height: 200,
-          //       child: const Text('Dummy bottom sheet'),
-          //     ),
-          //   ),
-          // ),
-          IconButton(icon: const Icon(Icons.search), onPressed: () {}
-              // Fluttertoast.showToast(msg: 'Dummy search action.'),
-              ),
           IconButton(
               alignment: Alignment.centerRight,
-              padding: const EdgeInsets.fromLTRB(300, 0, 0, 0),
-              icon: const Icon(Icons.more_vert),
-              onPressed:
-                  () {} //=> Fluttertoast.showToast(msg: 'Dummy menu action.'),
+              // padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+              icon: const Icon(
+                Icons.more_vert,
+                color: Colors.blue,
               ),
+              onPressed: () {}
+              //=> Fluttertoast.showToast(msg: 'Dummy menu action.'),
+              ),
+          // Text(
+          //   "BLOCO: " + contPage.toString(),
+          //   style: TextStyle(
+          //       fontWeight: FontWeight.w500,
+          //       color: Colors.black,
+          //       fontSize: 16.0),
+          //   textAlign: TextAlign.right,
+          // ),
         ],
       ),
     );
@@ -257,7 +313,6 @@ class _GeneralScrollViewWidgetState extends State<GeneralScrollViewWidget> {
         .toList();
     setState(() {
       listaCodigos = fields;
-      // fields = [];
     });
   }
 
@@ -284,51 +339,95 @@ class _GeneralScrollViewWidgetState extends State<GeneralScrollViewWidget> {
   addlist(int tan) {
     late List<List<dynamic>> codebar2;
     codebar2 = List<List<dynamic>>.empty(growable: true);
-    contPage = contPage + 1;
+    // contPage = contPage + 1;
     blocoCodigo = [];
-    tan = contPage * tan;
-    if (listaCodigos.isNotEmpty) {
-      for (int i = tan; i < tan + 10; i++) {
+    if (contPage == 0) {
+      tan = 0;
+    } else {
+      tan = (contPage * tan);
+    }
+    if (listaCodigos.isNotEmpty && tanBloc > 0) {
+      for (int i = tan; i < tan + tanBloc; i++) {
         listaCodigos[i][0] = listaCodigos[i][0].toString();
         codebar2.add(listaCodigos[i]);
       }
 
       setState(() {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < tanBloc; i++) {
           blocoCodigo.add(codebar2[i]);
         }
-        // if (tan > tanAnt) {
         contPage++;
-        // } else {
-        //   contPage--;
-        // }
       });
+    } else {
+      _showBloc("Sem quantidade definida!");
     }
   }
 
   remlist(int tan) {
     late List<List<dynamic>> codebar2;
     codebar2 = List<List<dynamic>>.empty(growable: true);
-    contPage = contPage - 1;
     blocoCodigo = [];
-    tan = contPage * tan;
-    if (listaCodigos.isNotEmpty) {
-      for (int i = tan; i < tan + 10; i++) {
+    if (contPage == 2) {
+      tan = 0;
+      contPage = 1;
+    } else {
+      contPage = contPage - 1;
+      tan = (contPage * tan) - 10;
+    }
+    if (listaCodigos.isNotEmpty && tanBloc > 0) {
+      for (int i = tan; i < tan + tanBloc; i++) {
         listaCodigos[i][0] = listaCodigos[i][0].toString();
         codebar2.add(listaCodigos[i]);
       }
 
       setState(() {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < tanBloc; i++) {
           blocoCodigo.add(codebar2[i]);
         }
-        // if (tan > tanAnt) {
-        //   contPage++;
-        // } else {
-        contPage = contPage - 1;
-        // }
+        contPage = contPage;
       });
+    } else {
+      // _showBloc();
     }
+  }
+
+  _onAlertWithCustomContentPressed(context) {
+    Alert(
+        context: context,
+        title: "Quantidade",
+        content: Column(
+          children: <Widget>[
+            // TextField(
+            //   decoration: InputDecoration(
+            //     icon: Icon(Icons.account_circle),
+            //     labelText: 'Username',
+            //   ),
+            // ),
+            TextField(
+              controller: ctrlSenha,
+              keyboardType: TextInputType.number,
+              obscureText: false,
+              decoration: InputDecoration(
+                icon: Icon(Icons.format_list_numbered_rounded),
+                labelText: 'Quantidade',
+              ),
+            ),
+          ],
+        ),
+        buttons: [
+          DialogButton(
+            onPressed: () {
+              // setState(() {
+              tanBloc = int.parse(ctrlSenha.text);
+              // });
+              Navigator.pop(context);
+            },
+            child: Text(
+              "Definir",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          )
+        ]).show();
   }
 
   void _showSnack() =>
@@ -342,6 +441,20 @@ class _GeneralScrollViewWidgetState extends State<GeneralScrollViewWidget> {
         ),
         duration: Duration(seconds: 2),
         backgroundColor: Colors.green,
+        dismissDirection: DismissDirection.up,
+      ));
+  void _showBloc(String mensager) =>
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(
+          mensager,
+          // "Sem quantidade Definida!",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+          ),
+        ),
+        duration: Duration(seconds: 2),
+        backgroundColor: Color.fromARGB(255, 106, 11, 11),
         dismissDirection: DismissDirection.up,
       ));
 
